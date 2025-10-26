eb32ac1c2737d9314be6096a036246656bd685f1
nskelley <nskelley9@gmail.com>
2025-10-25 23:52:42 -0400

    Code for combining BLS QCEW data

---
+++ 02progs/A02_combine_bls.R
@@ -0,0 +1,44 @@
+# ------------------------------------------------------------------------------
+# --------------------------- ECON6590 Final Project ---------------------------
+# --- Combine raw QCEW data from the BLS into a single, unified data set for ---
+# --- cleaning and analysis.                                                 ---
+# ------------------------------ Nicholas Skelley ------------------------------
+# ---------------------------- Created 25 Oct 2025 -----------------------------
+# ---------------------------- Updated 25 Oct 2025 -----------------------------
+# ------------------------------------------------------------------------------
+# Packages
+need <- c("here", "tidyverse", "arrow", "data.table")
+have <- need %in% rownames(installed.packages())
+if (any(!have)) install.packages(need[!have])
+invisible(lapply(need, library, character.only = TRUE))
+
+# WD default to detect script folder and then move as needed
+path <- rstudioapi::getSourceEditorContext()$path
+scriptFolder <- sub(".*/", "", dirname(path))
+scriptName <- basename(path)
+here::i_am(paste(scriptFolder, scriptName, sep = "/"))
+rm(list = ls())
+# ------------------------------------------------------------------------------
+
+# FIPS codes for states of interest
+state_fips <- c(17, 18, 21, 24, 26, 36, 39, 42, 47, 51, 54)
+state_pattern <- paste0("^(", paste0(state_fips, collapse = "|"), ")")
+# Years to keep
+years_keep <- 2006:2019
+
+ds <- open_dataset(here("01data/bls/combined/all_parquet"))
+
+lazy_filter <- ds |>
+  filter(year %in% years_keep,
+         grepl(state_pattern, area_fips),
+         !grepl("000$", area_fips)) |>
+  select(-any_of("qtr"))
+
+dt <- as.data.table(collect(lazy_filter))
+
+# Confirm level of data is year-county-industry-public/private
+nrow(dt) == nrow(unique(dt, by = c("year", "area_fips", "industry_code", 
+                                   "own_code")))
+
+fwrite(dt, here("01data/bls/combined/combined_qcew_raw.csv.gz"))
+
--- 02progs/zz_Explore.R
+++ 02progs/zz_Explore.R
@@ -3,10 +3,10 @@
 # --- Explore JOE data.                                                      ---
 # ------------------------------ Nicholas Skelley ------------------------------
 # ---------------------------- Created 23 Oct 2025 -----------------------------
-# ---------------------------- Updated 25 Oct 2025 -----------------------------
+# ---------------------------- Updated 24 Oct 2025 -----------------------------
 # ------------------------------------------------------------------------------
 # Packages
-need <- c("here", "tidyverse")
+need <- c("here", "tidyverse", "data.table")
 have <- need %in% rownames(installed.packages())
 if (any(!have)) install.packages(need[!have])
 invisible(lapply(need, library, character.only = TRUE))
@@ -19,6 +19,4 @@ here::i_am(paste(scriptFolder, scriptName, sep = "/"))
 rm(list = ls())
 # ------------------------------------------------------------------------------
 
-commute <- read_delim("01data/commutingzone/commutingzone_data.csv")
-
-
+test <- fread("https://www.bls.gov/cew/about-data/downloadable-file-layouts/quarterly/naics-based-quarterly-layout-csv.csv")
