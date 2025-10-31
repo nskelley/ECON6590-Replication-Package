03ce4400f6cff644a9259c25b8a5c12f6d3efa2e
nskelley <nskelley9@gmail.com>
2025-10-31 19:15:03 -0400

    Binscatters

--- 02progs/D01_naive_summaries.R
+++ 02progs/D01_naive_summaries.R
@@ -7,7 +7,7 @@
 # ---------------------------- Updated 31 Oct 2025 -----------------------------
 # ------------------------------------------------------------------------------
 # Packages
-need <- c("here", "tidyverse", "data.table", "binsreg")
+need <- c("here", "tidyverse", "data.table", "binsreg", "stargazer")
 have <- need %in% rownames(installed.packages())
 if (any(!have)) install.packages(need[!have])
 invisible(lapply(need, library, character.only = TRUE))
@@ -58,32 +58,53 @@ df_naif |>
   theme_bw() +
   theme(legend.position = "top")
 
-t <- df_naif |>
-  filter(off_type != 0) %>%
-  binsreg(x = oty_annual_avg_emplvl_pct_chg_2121,
-          y = mi_rate,
-          # w = select(., cohort_year, sex, race, age_group, fips),
-          data = .,
-          by = off_type,
-          # weight = coal_emp_share_2002,
-          randcut = 1,
-          nbins = 15,
-          line = c(1, 1))
+for (var in c("mi_rate", "fe_rate")) {
+  print(var)
+  # Limit data to valid rows
+  plot_base_df <- df_naif |>
+    mutate(out = get(var)) |>
+    filter(off_type != 0,
+           across(c(oty_annual_avg_emplvl_pct_chg_2121, off_type,
+                    coal_emp_share_2002, out), ~ !is.na(.)))
+  # Create binscatter/binsreg
+  .bs <- binsreg(x = oty_annual_avg_emplvl_pct_chg_2121,
+                 y = out,
+                 w = ~ cohort_year + sex + race + age_group + fips,
+                 data = plot_base_df,
+                 by = off_type,
+                 weight = coal_emp_share_2002,
+                 randcut = 1,
+                 binspos = "qs",
+                 nbins = 15,
+                 dots = TRUE)
+  # Get binscatter dots data for ggplot
+  .dots_df <- lapply(seq_along(.bs$data.plot), function(i) {
+    dp <- .bs$data.plot[[i]]$data.dots
+    if (is.null(dp)) return(NULL)
+    dp$by <- .bs$opt$byvals[i]
+    dp
+  }) |> bind_rows()
+  # Plot binscatter (manual)
+  .plot <- .dots_df |>
+    mutate(by = factor(by, levels = 1:3, labels = c("Violent", "Property", 
+                                                    "Drug"))) |>
+    ggplot(aes(x = x, y = fit, color = by)) +
+    geom_point(size = 2) +
+    scale_x_continuous(limits = c(-25, 25)) +
+    labs(x = "% change in coal employment share",
+         y = ifelse(var == "mi_rate",
+                    "Misdemanor Rate (per 100,000)",
+                    "Felony Rate (per 100,000)")) +
+    theme_minimal() +
+    guides(color = guide_legend(position = "bottom",
+                                title = "Offense Type")) +
+    theme(panel.grid.minor = element_blank())
+  
+          # panel.grid.major = element_line(linetype = "dashed", linewidth = 0.4),
+  ggsave(here("04work/early_figures", paste0("binscatter_", var, ".pdf")),
+         plot = .plot, width = 8, height = 6, units = "in")
+}
 
-df_naif |>
-  filter(off_type != 0) |>
-  ggplot(aes(x = oty_annual_avg_emplvl_pct_chg_2121,
-             y = mi_rate,
-             color = factor(off_type))) +
-  geom_smooth(method = "lm", se = FALSE) +
-  geom_point(data = t$data.plot, aes(x = x, y = ))
-
-t$bins_plot +
-  labs(x = "% change in average coal employment",
-       y = "Misdemeanor rate",
-       color = "Offense type") +
-  scale_x_continuous(limits = c(-50, 50)) +
-  theme(legend.position = "top")
 
 
 fwrite(df_naif, here("05prepdata/naive_tables_combined_data.csv"))
